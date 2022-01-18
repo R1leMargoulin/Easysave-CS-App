079e460da233c13cc00075fbd79f1d6f75c7bd2b
Victorien Goudeau <vgoudeau@sqli.com>
2022-01-18 09:56:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasySave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
