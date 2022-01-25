5b7c991a7385fd598747d617ec7b589be8b78491
Victorien Goudeau <vgoudeau@sqli.com>
2022-01-25 08:32:33 +0100

    add

---
+++ EasySaveTest/BackupTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasySaveTest
+{
+    [TestClass]
+    public class BackupTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
