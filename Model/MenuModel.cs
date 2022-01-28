8f7f9bf340aa64061e1d110611c69d03eece93e8
SimonJouenne <simon.jouenne@viacesi.fr>
2022-01-28 15:04:11 +0100

    Add menu

--- Model/MenuModel.cs
+++ Model/MenuModel.cs
@@ -7,39 +7,33 @@ namespace EasySave.Model
     public class MenuModel
     {
         public string language;
-        public List<string> languageList; 
-        private int menuView;
-        
+        public List<string> languageList;
+        private string menuView;
 
         public MenuModel()
         {
-            languageList = new List<string> { Language.fr.ToString(), Language.en.ToString()};
-            language = Convert.ToString(Language.fr);
-            menuView = 0;
+            languageList = new List<string> { Language.fr.ToString(), Language.en.ToString() };
+            language = Convert.ToString(Language.fr); //default language of the application
+            menuView = "0";
         }
 
-        public int GetMenuView()
+        public string GetMenuView()
         {
             return menuView;
         }
-        public void SetMenuView(int a)
+        public void SetMenuView(string a)
         {
             menuView = a;
         }
-        //public String GetLanguage()
-        //{
-        //    return language;
-        //}
 
         public void SetLanguage(string lang)
         {
             language = lang;
-           
         }
 
         public List<String> GetLanguageList()
         {
-           return languageList;
+            return languageList;
         }
     }
 }
