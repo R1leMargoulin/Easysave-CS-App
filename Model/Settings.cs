dd4c459f6996af54a46fbe37cf651dd4ffeb6758
Erwan MARTIN <erwan.martin@viacesi.fr>
2022-02-16 12:04:38 +0100

    commit Settings.cs into model

---
+++ Model/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Model
+{
+    class Settings
+    {
+        public String setting_language { get; set; }
+        public String setting_log { get; set; }
+        public String setting_process { get; set; }
+
+    }
+}
